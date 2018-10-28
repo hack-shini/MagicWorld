494686484f99f317de1bada15c63bc540ccd0ddd
Dimitri comenda <comenda.dimitri007@gmail.com>
2018-10-28 20:03:54 +0100

    Class Character -> abstract class Character Implémentation de la class

--- src/Character.java
+++ src/Character.java
@@ -1,2 +1,14 @@
-public class Character {
+public abstract class Character {
+
+    public abstract void basicAttack();
+
+    public abstract void specialAttack();
+
+    public abstract void warCry();
+
+    private int level;
+    private int life;
+    private int strength;
+    private int agility;
+    private int intelligence;
 }
